859d76e3cbc9478f62d6c223b8766984d26211f5
0Lipe <silvafelipes021@gmail.com>
2023-06-23 15:26:32 -0300

    commit inicial

---
+++ main.js
@@ -0,0 +1,34 @@
+const form = document.getElementById('form-contato');
+const Nomes = [];
+const numeros = [];
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    adicionaLinha();
+    atualizaTabela();
+});
+
+function adicionaLinha() {
+    const inputNomeContato = document.getElementById('nome-contato');
+    const inputNumeroContato = document.getElementById('numero-contato');
+
+    Nomes.push(inputNomeContato.value);
+    numeros.push(parseFloat(inputNumeroContato.value));
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeContato.value}</td>`;
+    linha += `<td>${inputNumeroContato.value}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    inputNomeContato.value = '';
+    inputNumeroContato.value = '';
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+}
